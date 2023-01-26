7c27e9bf82d0f62ea14d5b69347489a33677e3b5
Gökşen Ayar <111229793+Goksenns@users.noreply.github.com>
2023-01-27 00:15:21 +0300

    error messaging

--- programs/SocialFunding/src/state.rs
+++ programs/SocialFunding/src/state.rs
@@ -11,6 +11,7 @@ pub struct Management {
     pub donate_stage: i64,
     pub distribute_stage: i64,
 }
+
 #[derive(Accounts)]
 pub struct Stage<'info> {
     #[account(init,payer=admin,space=32+1+8+8+8)]
@@ -29,6 +30,7 @@ pub struct Pause<'info> {
     #[account(mut)]
     pub admin: Signer<'info>,
 }
+
 #[account]
 pub struct SolBank {
     pub amount: u64,
@@ -60,6 +62,7 @@ pub struct CreateCommunity<'info> {
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
+
 #[derive(Accounts)]
 pub struct JoinCommunity<'info> {
     #[account(mut)]
@@ -104,6 +107,7 @@ pub struct Project {
     pub description: String,
     pub executable: bool,
 }
+
 #[derive(Accounts)]
 pub struct CreateProject<'info> {
     #[account(init,payer=creator,space=8+32+32+8+4*50+4*200)]
@@ -164,11 +168,13 @@ impl VotingResult {
         }
     }
 }
+
 #[account]
 pub struct VoteCounter {
     pub yes_count: i64,
     pub no_count: i64,
 }
+
 #[derive(Accounts)]
 pub struct ExecuteProject<'info> {
     #[account(mut)]
